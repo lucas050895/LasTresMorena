9f1a67465c756531ae8321520b70c44eb61ce388
Lucas Conde <lucasconde742@gmail.com>
2024-05-23 10:12:56 -0300

    Nuevo layout para el footer

---
+++ layout/footer.php
@@ -0,0 +1,31 @@
+<footer>
+        <div class="redes">
+            <p>Comunicate con nosotros</p>
+            <div>
+                <a href="#">
+                    <i class="fab fa-facebook"></i>
+                </a>
+
+                <a href="#">
+                    <i class="fab fa-instagram-square"></i>
+                </a>
+
+                <a href="tel:123456">
+                    <i class="fas fa-phone-alt"></i>
+                </a>
+
+                <a href="#">
+                    <i class="fas fa-envelope"></i>
+                </a>
+            </div>
+        </div>
+        <div>
+            <h2>
+                Las Tres Morenas
+            </h2>
+            <p>
+                <i class="far fa-copyright"></i>
+                Todos Los Derechos reservados
+            </p>
+        </div>
+    </footer>
