ec5e508962c48b2ee297982edcc818afb6c0bd02
Lucas Conde <lucasconde742@gmail.com>
2024-05-31 15:33:50 -0300

    Modificaciones en el link de busqueda

--- links/busqueda.php
+++ links/busqueda.php
@@ -1,63 +1,41 @@
 <?php
     include('../bd/conecxion.php');
-
-    $where= "";
-    if(!empty($_POST)){
-        $valor = $_POST['buscar'];
-
-        if(!empty($valor)){
-            $where = "WHERE descripcion LIKE '$valor%' ";
-        }
-    }
-    $consulta = "SELECT * FROM articulos $where";
-
-    $resultado = $conexion->query($consulta);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <link rel="stylesheet" href="../css/general.css">
-
-    <script src="https://kit.fontawesome.com/439ee37b3b.js" crossorigin="anonymous"></script>
-</head>
+<html lang="es">
+    <head> 
+        <!-- CHARSET -->
+        <meta charset="UTF-8">
+        <!-- IE-EDGE -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <!-- VIEWPORT -->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <!-- DESCRIPTION -->
+        <meta name="description" content="Sitio web de artículos y accesorios de pesca.">
+        <!-- AUTHOR -->
+        <meta name="author" content="Lucas Conde">
+        <!-- TITTLE -->
+        <title>Las Tres Morena</title>
+        <!-- STYLES -->
+        <link rel="stylesheet" href="../css/familia.css">
+        <!-- FONT AWESOME -->
+        <script src="https://kit.fontawesome.com/439ee37b3b.js" crossorigin="anonymous"></script>
+    </head>
 <body>
+    <?php
+        include('../layout/nav.php');
+    ?>
 
-    <nav>
-        <div>
-            <i class="fas fa-bars"></i>
-            <h1>Las Tres Morenas</h1>
-            <i class="fas fa-search" onclick="MostrarOcultar()"></i>
-        </div>
+    <main>
+        <form action="busqueda1.php" method="GET">
+            <input type="text" name="busqueda" placeholder="Busqueda de articulo.." required>
+            <input type="submit" value="Buscar">
+        </form>
+    </main>
 
-        <div id="ocultar">
-            <form action="busqueda.php" method="post">
-                <label for="search">Busqueda : </label>
-                <input type="text" name="buscar" id="search" class="search" required>
-                <input type="submit" value="Buscar">
-            </form>
-        </div>
-    </nav>
-
-    <div>
-        <?php
-            while($row = $resultado->fetch_assoc()){
-                ?>
-                    <h2>
-                        <?php
-                            echo $row['codigo'];
-                            echo $row['descripcion'];
-                        ?>
-                    </h2>
-                <?php
-            }
-        ?>
-    </div>
-
-    <script src="../js/index.js"></script>
-</body>   
+    <?php
+        include('../layout/footer.php');
+    ?>
+</body>
 </html>
