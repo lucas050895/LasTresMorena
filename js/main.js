0b7abd0c22df1b1da4f13742ddadf212ba14c4d0
Lucas Conde <lucasconde742@gmail.com>
2024-07-02 15:12:07 -0300

    Se agrego y cambio efecto Slider en index

--- js/main.js
+++ js/main.js
@@ -35,33 +35,44 @@ window.onscroll = function(){
 
 
 //  SLIDER AUTOMATICO DE INDEX
-let slider = document.querySelector(".slider-contenedor")
-let sliderIndividual = document.querySelectorAll(".contenido-slider")
-let contador = 1;
-let width = sliderIndividual[0];
-let intervalo = 3000;
+const imgs = document.querySelectorAll(".container img");
+const dots = document.querySelectorAll(".dot i");
+const leftArrow = document.querySelector(".arrow-left");
+const rightArrow = document.querySelector(".arrow-right");
 
-window.addEventListener("resize", function(){
-    width = sliderIndividual[0].clientWidth;
-})
+let currentIndex = 0;
+let time = 5000; // default time for auto slideshow
 
-setInterval(function(){
-    slides();
-},intervalo);
-
-function slides(){
-    slider.style.transform = "translate("+(-width*contador)+"px)";
-    slider.style.transition = "transform .8s";
-    contador++;
-
-    if(contador == sliderIndividual.length){
-        setTimeout(function(){
-            slider.style.transform = "translate(0px)";
-            slider.style.transition = "transform 0s";
-            contador=1;
-        },1500)
-    }
+const defClass = (startPos, index) => {
+for (let i = startPos; i < imgs.length; i++) {
+    imgs[i].style.display = "none";
+    dots[i].classList.remove("fa-dot-circle");
+    dots[i].classList.add("fa-circle");
 }
+    imgs[index].style.display = "block";
+    dots[index].classList.add("fa-dot-circle");
+};
+
+defClass(1, 0);
+
+leftArrow.addEventListener("click", function(){
+    currentIndex <= 0 ? currentIndex = imgs.length-1 : currentIndex--;
+    defClass(0, currentIndex);
+});
+
+rightArrow.addEventListener("click", function(){
+    currentIndex >= imgs.length-1 ? currentIndex = 0 : currentIndex++;
+    defClass(0, currentIndex);
+});
+
+const startAutoSlide = () => {
+    setInterval(() => {
+        currentIndex >= imgs.length-1 ? currentIndex = 0 : currentIndex++;
+        defClass(0, currentIndex);
+    }, time);
+};
+
+startAutoSlide();
 
 
 
